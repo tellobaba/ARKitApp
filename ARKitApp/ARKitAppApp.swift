44b6862340c7a28407ff019327f527bf051d701c
tellobaba <olatunjitello17@gmail.com>
2023-04-25 07:57:42 +0100

    Initial Commit

---
+++ ARKitApp/ARKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARKitAppApp.swift
+//  ARKitApp
+//
+//  Created by Oluwatomiwa on 25/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ARKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
